620e7f6d7ae10ab41f5e744447cd38bb076a8225
Hakan Baba <hbaba@mbp-005063.ad.whirl.net>
2016-09-16 17:52:13 -0700

    Separated the metrics registry from the DefaultRegisty in prometheus. Previously the kube-state-metrics (ksm) was using the default registry from prometheus client inteface. The problem with that was: The default registry already contains collectors specific to the process. That resulted in bunch of unnecessary metrics in the output. With this change we separate the registry used for the metrics from the default registry. This also resolves issue #20

--- main.go
+++ main.go
@@ -76,8 +76,11 @@ func main() {
 		glog.Fatalf("Failed to create client: %v", err)
 	}
 
-	initializeMetrics(kubeClient)
-	metricsServer()
+	// The speficic prometheus registry for the metrics
+	reg := prometheus.NewRegistry()
+
+	initializeMetrics(kubeClient, reg)
+	metricsServer(reg)
 }
 
 func createKubeClient() (kubeClient clientset.Interface, err error) {
@@ -131,13 +134,15 @@ func createKubeClient() (kubeClient clientset.Interface, err error) {
 	return kubeClient, nil
 }
 
-func metricsServer() {
+func metricsServer(reg prometheus.Gatherer) {
 	// Address to listen on for web interface and telemetry
 	listenAddress := fmt.Sprintf(":%d", *port)
 
 	glog.Infof("Starting metrics server: %s", listenAddress)
+
 	// Add metricsPath
-	http.Handle(metricsPath, prometheus.UninstrumentedHandler())
+	http.Handle(metricsPath, promhttp.HandlerFor(reg, promhttp.HandlerOpts{}))
+
 	// Add healthzPath
 	http.HandleFunc(healthzPath, func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(200)
@@ -178,7 +183,7 @@ func (l NodeLister) List() (v1.NodeList, error) {
 }
 
 // initializeMetrics creates a new controller from the given config.
-func initializeMetrics(kubeClient clientset.Interface) {
+func initializeMetrics(kubeClient clientset.Interface, reg prometheus.Registerer) {
 	dplStore, dplController := cache.NewNamespaceKeyedIndexerAndReflector(
 		&cache.ListWatch{
 			ListFunc: func(options api.ListOptions) (runtime.Object, error) {
@@ -230,12 +235,7 @@ func initializeMetrics(kubeClient clientset.Interface) {
 		return machines, nil
 	})
 
-	_ = promhttp.Handler()
-	// FixMe: This change line is only there for the go compiler not to compile.
-	// We first wanted to set the godeps correctly to include the promhttp in the
-	// vendor directory. In the following changes I will actually use the promhttp
-
-	prometheus.MustRegister(&deploymentCollector{store: dplLister})
-	prometheus.MustRegister(&podCollector{store: podLister})
-	prometheus.MustRegister(&nodeCollector{store: nodeLister})
+	reg.MustRegister(&deploymentCollector{store: dplLister})
+	reg.MustRegister(&podCollector{store: podLister})
+	reg.MustRegister(&nodeCollector{store: nodeLister})
 }

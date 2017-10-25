1b6f3552637eacf437b5406aa866549175359b2d
zouyee <zouyee1989@gmail.com>
2017-10-25 15:54:33 +0800

    fix hpa nil error

--- collectors/hpa.go
+++ collectors/hpa.go
@@ -23,6 +23,7 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 	autoscaling "k8s.io/api/autoscaling/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/fields"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/cache"
 )
@@ -72,7 +73,8 @@ func (l HPALister) List() (autoscaling.HorizontalPodAutoscalerList, error) {
 
 func RegisterHorizontalPodAutoScalerCollector(registry prometheus.Registerer, kubeClient kubernetes.Interface, namespace string) {
 	client := kubeClient.Autoscaling().RESTClient()
-	hpalw := cache.NewListWatchFromClient(client, "horizontalpodautoscalers", metav1.NamespaceAll, nil)
+	glog.Infof("collect hpa with %s", client.APIVersion())
+	hpalw := cache.NewListWatchFromClient(client, "horizontalpodautoscalers", metav1.NamespaceAll, fields.Everything())
 	hpainf := cache.NewSharedInformer(hpalw, &autoscaling.HorizontalPodAutoscaler{}, resyncPeriod)
 
 	hpaLister := HPALister(func() (hpas autoscaling.HorizontalPodAutoscalerList, err error) {

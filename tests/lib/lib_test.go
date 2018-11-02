5a8b86b1c7e0d1c183e5b988e99d2c3b3a2cea06
Lili Cosic <cosiclili@gmail.com>
2018-11-02 12:15:37 +0100

    tests: Add test to test k-s-m as a standalone library
    With the different pieces being exposed, this test ensures that k-s-m
    can be used as a standalone library.
    
    Co-authored-by: Max Leonard Inden <IndenML@gmail.com>

---
+++ tests/lib/lib_test.go
@@ -0,0 +1,81 @@
+package lib
+
+import (
+	"context"
+	"strings"
+	"testing"
+	"time"
+
+	"k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/watch"
+	clientset "k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/kubernetes/fake"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/kube-state-metrics/pkg/collectors"
+	"k8s.io/kube-state-metrics/pkg/metrics"
+	metricsstore "k8s.io/kube-state-metrics/pkg/metrics_store"
+)
+
+func TestAsLibrary(t *testing.T) {
+	kubeClient := fake.NewSimpleClientset()
+
+	service := v1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:            "my-service",
+			ResourceVersion: "123456",
+		},
+	}
+
+	_, err := kubeClient.CoreV1().Services(metav1.NamespaceDefault).Create(&service)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	c := serviceCollector(kubeClient)
+
+	// Wait for informers to sync
+	time.Sleep(time.Second)
+
+	m := c.Collect()
+
+	if len(m) != 1 {
+		t.Fatalf("expected one metric to be returned but got %v", len(m))
+	}
+
+	if !strings.Contains(string(*m[0]), service.ObjectMeta.Name) {
+		t.Fatal("expected string to contain service name")
+	}
+}
+
+func serviceCollector(kubeClient clientset.Interface) *collectors.Collector {
+	store := metricsstore.NewMetricsStore(generateServiceMetrics)
+
+	lw := cache.ListWatch{
+		ListFunc: func(opts metav1.ListOptions) (runtime.Object, error) {
+			return kubeClient.CoreV1().Services(metav1.NamespaceDefault).List(opts)
+		},
+		WatchFunc: func(opts metav1.ListOptions) (watch.Interface, error) {
+			return kubeClient.CoreV1().Services(metav1.NamespaceDefault).Watch(opts)
+		},
+	}
+
+	r := cache.NewReflector(&lw, &v1.Service{}, store, 0)
+
+	go r.Run(context.TODO().Done())
+
+	return collectors.NewCollector(store)
+}
+
+func generateServiceMetrics(obj interface{}) []*metrics.Metric {
+	sPointer := obj.(*v1.Service)
+	s := *sPointer
+
+	m, err := metrics.NewMetric("test_metric", []string{"name"}, []string{s.Name}, 1)
+	if err != nil {
+		panic(err)
+	}
+
+	return []*metrics.Metric{m}
+}

7b6e7cc604660267f88e44e825097af68691c9bf
Max Leonard Inden <IndenML@gmail.com>
2018-11-22 17:38:06 +0100

    pkg/collectors: Refactor deployment collector

--- main_test.go
+++ main_test.go
@@ -154,6 +154,18 @@ func TestFullScrapeCycle(t *testing.T) {
 # HELP kube_daemonset_updated_number_scheduled The total number of nodes that are running updated daemon pod
 # HELP kube_daemonset_metadata_generation Sequence number representing a specific generation of the desired state.
 # HELP kube_daemonset_labels Kubernetes labels converted to Prometheus labels.
+# HELP kube_deployment_created Unix creation timestamp
+# HELP kube_deployment_status_replicas The number of replicas per deployment.
+# HELP kube_deployment_status_replicas_available The number of available replicas per deployment.
+# HELP kube_deployment_status_replicas_unavailable The number of unavailable replicas per deployment.
+# HELP kube_deployment_status_replicas_updated The number of updated replicas per deployment.
+# HELP kube_deployment_status_observed_generation The generation observed by the deployment controller.
+# HELP kube_deployment_spec_replicas Number of desired pods for a deployment.
+# HELP kube_deployment_spec_paused Whether the deployment is paused and will not be processed by the deployment controller.
+# HELP kube_deployment_spec_strategy_rollingupdate_max_unavailable Maximum number of unavailable replicas during a rolling update of a deployment.
+# HELP kube_deployment_spec_strategy_rollingupdate_max_surge Maximum number of replicas that can be scheduled above the desired number of replicas during a rolling update of a deployment.
+# HELP kube_deployment_metadata_generation Sequence number representing a specific generation of the desired state.
+# HELP kube_deployment_labels Kubernetes labels converted to Prometheus labels.
 # HELP kube_pod_info Information about pod.
 # HELP kube_pod_start_time Start time in unix timestamp for a pod.
 # HELP kube_pod_completion_time Completion time in unix timestamp for a pod.

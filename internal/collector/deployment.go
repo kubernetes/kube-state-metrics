938f6ce5efe85ccf927ebdfcda458725c8fd3d68
Lili Cosic <cosiclili@gmail.com>
2019-01-11 14:21:30 +0100

    *: Move collectors pkg to internal directory
    This makes the rest of the packages useful to be used in a standalone
    library without importing the kube-state-metrics specific collectors.
    
    * Rename collectors -> collector package
    
    * Rename metrics -> metric package
    
    * Add metricFamily mocking in tests to prevent cyclic dependency.

---
+++ internal/collector/deployment.go
@@ -0,0 +1,223 @@
+/*
+Copyright 2016 The Kubernetes Authors All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package collector
+
+import (
+	"k8s.io/kube-state-metrics/pkg/metric"
+
+	"k8s.io/api/extensions/v1beta1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/util/intstr"
+	"k8s.io/apimachinery/pkg/watch"
+	clientset "k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/cache"
+)
+
+var (
+	descDeploymentLabelsName          = "kube_deployment_labels"
+	descDeploymentLabelsHelp          = "Kubernetes labels converted to Prometheus labels."
+	descDeploymentLabelsDefaultLabels = []string{"namespace", "deployment"}
+
+	deploymentMetricFamilies = []metric.FamilyGenerator{
+		{
+			Name: "kube_deployment_created",
+			Type: metric.MetricTypeGauge,
+			Help: "Unix creation timestamp",
+			GenerateFunc: wrapDeploymentFunc(func(d *v1beta1.Deployment) metric.Family {
+				f := metric.Family{}
+
+				if !d.CreationTimestamp.IsZero() {
+					f = append(f, &metric.Metric{
+						Name:  "kube_deployment_created",
+						Value: float64(d.CreationTimestamp.Unix()),
+					})
+				}
+
+				return f
+			}),
+		},
+		{
+			Name: "kube_deployment_status_replicas",
+			Type: metric.MetricTypeGauge,
+			Help: "The number of replicas per deployment.",
+			GenerateFunc: wrapDeploymentFunc(func(d *v1beta1.Deployment) metric.Family {
+				return metric.Family{&metric.Metric{
+					Name:  "kube_deployment_status_replicas",
+					Value: float64(d.Status.Replicas),
+				}}
+			}),
+		},
+		{
+			Name: "kube_deployment_status_replicas_available",
+			Type: metric.MetricTypeGauge,
+			Help: "The number of available replicas per deployment.",
+			GenerateFunc: wrapDeploymentFunc(func(d *v1beta1.Deployment) metric.Family {
+				return metric.Family{&metric.Metric{
+					Name:  "kube_deployment_status_replicas_available",
+					Value: float64(d.Status.AvailableReplicas),
+				}}
+			}),
+		},
+		{
+			Name: "kube_deployment_status_replicas_unavailable",
+			Type: metric.MetricTypeGauge,
+			Help: "The number of unavailable replicas per deployment.",
+			GenerateFunc: wrapDeploymentFunc(func(d *v1beta1.Deployment) metric.Family {
+				return metric.Family{&metric.Metric{
+					Name:  "kube_deployment_status_replicas_unavailable",
+					Value: float64(d.Status.UnavailableReplicas),
+				}}
+			}),
+		},
+		{
+			Name: "kube_deployment_status_replicas_updated",
+			Type: metric.MetricTypeGauge,
+			Help: "The number of updated replicas per deployment.",
+			GenerateFunc: wrapDeploymentFunc(func(d *v1beta1.Deployment) metric.Family {
+				return metric.Family{&metric.Metric{
+					Name:  "kube_deployment_status_replicas_updated",
+					Value: float64(d.Status.UpdatedReplicas),
+				}}
+			}),
+		},
+		{
+			Name: "kube_deployment_status_observed_generation",
+			Type: metric.MetricTypeGauge,
+			Help: "The generation observed by the deployment controller.",
+			GenerateFunc: wrapDeploymentFunc(func(d *v1beta1.Deployment) metric.Family {
+				return metric.Family{&metric.Metric{
+					Name:  "kube_deployment_status_observed_generation",
+					Value: float64(d.Status.ObservedGeneration),
+				}}
+			}),
+		},
+		{
+			Name: "kube_deployment_spec_replicas",
+			Type: metric.MetricTypeGauge,
+			Help: "Number of desired pods for a deployment.",
+			GenerateFunc: wrapDeploymentFunc(func(d *v1beta1.Deployment) metric.Family {
+				return metric.Family{&metric.Metric{
+					Name:  "kube_deployment_spec_replicas",
+					Value: float64(*d.Spec.Replicas),
+				}}
+			}),
+		},
+		{
+			Name: "kube_deployment_spec_paused",
+			Type: metric.MetricTypeGauge,
+			Help: "Whether the deployment is paused and will not be processed by the deployment controller.",
+			GenerateFunc: wrapDeploymentFunc(func(d *v1beta1.Deployment) metric.Family {
+				return metric.Family{&metric.Metric{
+					Name:  "kube_deployment_spec_paused",
+					Value: boolFloat64(d.Spec.Paused),
+				}}
+			}),
+		},
+		{
+			Name: "kube_deployment_spec_strategy_rollingupdate_max_unavailable",
+			Type: metric.MetricTypeGauge,
+			Help: "Maximum number of unavailable replicas during a rolling update of a deployment.",
+			GenerateFunc: wrapDeploymentFunc(func(d *v1beta1.Deployment) metric.Family {
+				if d.Spec.Strategy.RollingUpdate == nil {
+					return metric.Family{}
+				}
+
+				maxUnavailable, err := intstr.GetValueFromIntOrPercent(d.Spec.Strategy.RollingUpdate.MaxUnavailable, int(*d.Spec.Replicas), true)
+				if err != nil {
+					panic(err)
+				}
+
+				return metric.Family{&metric.Metric{
+					Name:  "kube_deployment_spec_strategy_rollingupdate_max_unavailable",
+					Value: float64(maxUnavailable),
+				}}
+			}),
+		},
+		{
+			Name: "kube_deployment_spec_strategy_rollingupdate_max_surge",
+			Type: metric.MetricTypeGauge,
+			Help: "Maximum number of replicas that can be scheduled above the desired number of replicas during a rolling update of a deployment.",
+			GenerateFunc: wrapDeploymentFunc(func(d *v1beta1.Deployment) metric.Family {
+				if d.Spec.Strategy.RollingUpdate == nil {
+					return metric.Family{}
+				}
+
+				maxSurge, err := intstr.GetValueFromIntOrPercent(d.Spec.Strategy.RollingUpdate.MaxSurge, int(*d.Spec.Replicas), true)
+				if err != nil {
+					panic(err)
+				}
+
+				return metric.Family{&metric.Metric{
+					Name:  "kube_deployment_spec_strategy_rollingupdate_max_surge",
+					Value: float64(maxSurge),
+				}}
+			}),
+		},
+		{
+			Name: "kube_deployment_metadata_generation",
+			Type: metric.MetricTypeGauge,
+			Help: "Sequence number representing a specific generation of the desired state.",
+			GenerateFunc: wrapDeploymentFunc(func(d *v1beta1.Deployment) metric.Family {
+				return metric.Family{&metric.Metric{
+					Name:  "kube_deployment_metadata_generation",
+					Value: float64(d.ObjectMeta.Generation),
+				}}
+			}),
+		},
+		{
+			Name: descDeploymentLabelsName,
+			Type: metric.MetricTypeGauge,
+			Help: descDeploymentLabelsHelp,
+			GenerateFunc: wrapDeploymentFunc(func(d *v1beta1.Deployment) metric.Family {
+				labelKeys, labelValues := kubeLabelsToPrometheusLabels(d.Labels)
+				return metric.Family{&metric.Metric{
+					Name:        descDeploymentLabelsName,
+					LabelKeys:   labelKeys,
+					LabelValues: labelValues,
+					Value:       1,
+				}}
+			}),
+		},
+	}
+)
+
+func wrapDeploymentFunc(f func(*v1beta1.Deployment) metric.Family) func(interface{}) metric.Family {
+	return func(obj interface{}) metric.Family {
+		deployment := obj.(*v1beta1.Deployment)
+
+		metricFamily := f(deployment)
+
+		for _, m := range metricFamily {
+			m.LabelKeys = append(descDeploymentLabelsDefaultLabels, m.LabelKeys...)
+			m.LabelValues = append([]string{deployment.Namespace, deployment.Name}, m.LabelValues...)
+		}
+
+		return metricFamily
+	}
+}
+
+func createDeploymentListWatch(kubeClient clientset.Interface, ns string) cache.ListWatch {
+	return cache.ListWatch{
+		ListFunc: func(opts metav1.ListOptions) (runtime.Object, error) {
+			return kubeClient.ExtensionsV1beta1().Deployments(ns).List(opts)
+		},
+		WatchFunc: func(opts metav1.ListOptions) (watch.Interface, error) {
+			return kubeClient.ExtensionsV1beta1().Deployments(ns).Watch(opts)
+		},
+	}
+}
---
+++ internal/collector/persistentvolumeclaim.go
@@ -0,0 +1,156 @@
+/*
+Copyright 2017 The Kubernetes Authors All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package collector
+
+import (
+	"k8s.io/kube-state-metrics/pkg/metric"
+
+	"k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/watch"
+	clientset "k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/cache"
+)
+
+var (
+	descPersistentVolumeClaimLabelsName          = "kube_persistentvolumeclaim_labels"
+	descPersistentVolumeClaimLabelsHelp          = "Kubernetes labels converted to Prometheus labels."
+	descPersistentVolumeClaimLabelsDefaultLabels = []string{"namespace", "persistentvolumeclaim"}
+
+	persistentVolumeClaimMetricFamilies = []metric.FamilyGenerator{
+		{
+			Name: descPersistentVolumeClaimLabelsName,
+			Type: metric.MetricTypeGauge,
+			Help: descPersistentVolumeClaimLabelsHelp,
+			GenerateFunc: wrapPersistentVolumeClaimFunc(func(p *v1.PersistentVolumeClaim) metric.Family {
+				labelKeys, labelValues := kubeLabelsToPrometheusLabels(p.Labels)
+				return metric.Family{&metric.Metric{
+					Name:        descPersistentVolumeClaimLabelsName,
+					LabelKeys:   labelKeys,
+					LabelValues: labelValues,
+					Value:       1,
+				}}
+			}),
+		},
+		{
+			Name: "kube_persistentvolumeclaim_info",
+			Type: metric.MetricTypeGauge,
+			Help: "Information about persistent volume claim.",
+			GenerateFunc: wrapPersistentVolumeClaimFunc(func(p *v1.PersistentVolumeClaim) metric.Family {
+				storageClassName := getPersistentVolumeClaimClass(p)
+				volumeName := p.Spec.VolumeName
+				return metric.Family{&metric.Metric{
+					Name:        "kube_persistentvolumeclaim_info",
+					LabelKeys:   []string{"storageclass", "volumename"},
+					LabelValues: []string{storageClassName, volumeName},
+					Value:       1,
+				}}
+			}),
+		},
+		{
+			Name: "kube_persistentvolumeclaim_status_phase",
+			Type: metric.MetricTypeGauge,
+			Help: "The phase the persistent volume claim is currently in.",
+			GenerateFunc: wrapPersistentVolumeClaimFunc(func(p *v1.PersistentVolumeClaim) metric.Family {
+				f := metric.Family{}
+				// Set current phase to 1, others to 0 if it is set.
+				if p := p.Status.Phase; p != "" {
+					f = append(f,
+						&metric.Metric{
+							LabelValues: []string{string(v1.ClaimLost)},
+							Value:       boolFloat64(p == v1.ClaimLost),
+						},
+						&metric.Metric{
+							LabelValues: []string{string(v1.ClaimBound)},
+							Value:       boolFloat64(p == v1.ClaimBound),
+						},
+						&metric.Metric{
+							LabelValues: []string{string(v1.ClaimPending)},
+							Value:       boolFloat64(p == v1.ClaimPending),
+						},
+					)
+				}
+
+				for _, m := range f {
+					m.Name = "kube_persistentvolumeclaim_status_phase"
+					m.LabelKeys = []string{"phase"}
+				}
+
+				return f
+			}),
+		},
+		{
+			Name: "kube_persistentvolumeclaim_resource_requests_storage_bytes",
+			Type: metric.MetricTypeGauge,
+			Help: "The capacity of storage requested by the persistent volume claim.",
+			GenerateFunc: wrapPersistentVolumeClaimFunc(func(p *v1.PersistentVolumeClaim) metric.Family {
+				f := metric.Family{}
+				if storage, ok := p.Spec.Resources.Requests[v1.ResourceStorage]; ok {
+					f = append(f, &metric.Metric{
+						Name:  "kube_persistentvolumeclaim_resource_requests_storage_bytes",
+						Value: float64(storage.Value()),
+					})
+				}
+
+				return f
+			}),
+		},
+	}
+)
+
+func wrapPersistentVolumeClaimFunc(f func(*v1.PersistentVolumeClaim) metric.Family) func(interface{}) metric.Family {
+	return func(obj interface{}) metric.Family {
+		persistentVolumeClaim := obj.(*v1.PersistentVolumeClaim)
+
+		metricFamily := f(persistentVolumeClaim)
+
+		for _, m := range metricFamily {
+			m.LabelKeys = append(descPersistentVolumeClaimLabelsDefaultLabels, m.LabelKeys...)
+			m.LabelValues = append([]string{persistentVolumeClaim.Namespace, persistentVolumeClaim.Name}, m.LabelValues...)
+		}
+
+		return metricFamily
+	}
+}
+
+func createPersistentVolumeClaimListWatch(kubeClient clientset.Interface, ns string) cache.ListWatch {
+	return cache.ListWatch{
+		ListFunc: func(opts metav1.ListOptions) (runtime.Object, error) {
+			return kubeClient.CoreV1().PersistentVolumeClaims(ns).List(opts)
+		},
+		WatchFunc: func(opts metav1.ListOptions) (watch.Interface, error) {
+			return kubeClient.CoreV1().PersistentVolumeClaims(ns).Watch(opts)
+		},
+	}
+}
+
+// getPersistentVolumeClaimClass returns StorageClassName. If no storage class was
+// requested, it returns "".
+func getPersistentVolumeClaimClass(claim *v1.PersistentVolumeClaim) string {
+	// Use beta annotation first
+	if class, found := claim.Annotations[v1.BetaStorageClassAnnotation]; found {
+		return class
+	}
+
+	if claim.Spec.StorageClassName != nil {
+		return *claim.Spec.StorageClassName
+	}
+
+	// Special non-empty string to indicate absence of storage class.
+	return "<none>"
+}

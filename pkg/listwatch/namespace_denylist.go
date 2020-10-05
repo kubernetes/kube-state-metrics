4093cb34de2b6dc2f3c47ea54ae45a2416197013
Lili Cosic <cosiclili@gmail.com>
2020-10-05 15:28:50 +0200

    Adjust klog to klog/v2

--- pkg/listwatch/namespace_denylist.go
+++ pkg/listwatch/namespace_denylist.go
@@ -25,7 +25,7 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/tools/cache"
-	"k8s.io/klog"
+	"k8s.io/klog/v2"
 )
 
 // denylistListerWatcher implements cache.ListerWatcher

71f4162c9ca6b9f8c845612dc2505e1325d9d3d2
Julian van den Berkmortel <julianvdberkmortel@outlook.com>
2021-12-08 20:55:01 +0100

    Implement (namespace_list.NamespaceList).String according to the behaviour of the test

--- pkg/options/namespace_list/types.go
+++ pkg/options/namespace_list/types.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2018 The Kubernetes Authors All rights reserved.
+Copyright 2021 The Kubernetes Authors All rights reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -14,21 +14,43 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package namespace_list
+package namespacelist
+
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
 
 // NamespaceList represents a list of namespaces by name and labels.
 type NamespaceList map[string]map[string]string
 
+// String returns the map of namespaces and labels as a string
 func (n *NamespaceList) String() string {
-	// TODO: implement the receiver function
-	return ""
+	namespaces := make([]string, 0, len(*n))
+	for namespace, labels := range *n {
+		if len(labels) == 0 {
+			namespaces = append(namespaces, namespace)
+		} else {
+			concatenatedLabelValues := make([]string, 0, len(labels))
+			for label, value := range labels {
+				concatenatedLabelValues = append(concatenatedLabelValues, fmt.Sprintf("%v=%v", label, value))
+			}
+			sort.Strings(concatenatedLabelValues)
+			namespaces = append(namespaces, fmt.Sprintf("%v=[%v]", namespace, strings.Join(concatenatedLabelValues, ",")))
+		}
+	}
+	sort.Strings(namespaces)
+	return strings.Join(namespaces, ",")
 }
 
+// Set converts a comma-separated string of namespaces and labels into a map.
 func (n *NamespaceList) Set(value string) error {
 	// TODO: implement the receiver function
 	return nil
 }
 
+// Type returns a descriptive string about the NamespaceList type.
 func (n *NamespaceList) Type() string {
 	return "string"
 }
--- pkg/options/namespace_list/types_test.go
+++ pkg/options/namespace_list/types_test.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2018 The Kubernetes Authors All rights reserved.
+Copyright 2021 The Kubernetes Authors All rights reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package namespace_list
+package namespacelist
 
 import (
 	"reflect"

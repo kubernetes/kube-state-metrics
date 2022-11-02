3d8abf894b8b63c762ef1c2d78778d1a4422e0a3
Manuel Rüger <manuel@rueg.eu>
2022-11-02 11:25:42 +0100

    e2e: Test against k8s 1.25.0
    Signed-off-by: Manuel Rüger <manuel@rueg.eu>

--- tests/e2e.sh
+++ tests/e2e.sh
@@ -24,13 +24,13 @@ case $(uname -m) in
 esac
 
 NODE_IMAGE_NAME="docker.io/kindest/node"
-KUBERNETES_VERSION=${KUBERNETES_VERSION:-"v1.24.0"}
+KUBERNETES_VERSION=${KUBERNETES_VERSION:-"v1.25.0"}
 KUBE_STATE_METRICS_LOG_DIR=./log
 KUBE_STATE_METRICS_CURRENT_IMAGE_NAME="registry.k8s.io/kube-state-metrics/kube-state-metrics"
 KUBE_STATE_METRICS_IMAGE_NAME="registry.k8s.io/kube-state-metrics/kube-state-metrics-${ARCH}"
 E2E_SETUP_KIND=${E2E_SETUP_KIND:-}
 E2E_SETUP_KUBECTL=${E2E_SETUP_KUBECTL:-}
-KIND_VERSION=v0.14.0
+KIND_VERSION=v0.17.0
 SUDO=${SUDO:-}
 
 OS=$(uname -s | awk '{print tolower($0)}')

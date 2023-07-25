206918bec203299621462d1cd0c2474c71879ebf
QIZHANG123127 <62197641+QIZHANG123127@users.noreply.github.com>
2023-07-25 15:02:29 +0800

    Add Func Get_Instance_IP_Addr

--- pkg/options/resource.go
+++ pkg/options/resource.go
@@ -23,7 +23,6 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"qoobing.com/gomod/log"
 )
-
 var (
 	// DefaultNamespaces is the default namespace selector for selecting and filtering across all namespaces.
 	DefaultNamespaces = NamespaceList{metav1.NamespaceAll}

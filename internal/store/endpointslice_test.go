f3698288ff7061ab78522520c07d4d009db4dd50
Mark Robinson <mark@mrobinson.ca>
2023-08-31 07:33:20 -0700

    feat: Add support for endpoint topology routing hints (#2090)
    * Add support for endpoint topology routing hints
    
    Fix goimports
    
    Fix gocritic issue
    
    * Update docs/endpointslice-metrics.md
    
    Co-authored-by: Manuel Rüger <manuel@rueg.eu>
    
    * Update internal/store/endpointslice.go
    
    Co-authored-by: Manuel Rüger <manuel@rueg.eu>
    
    * Update Dockerfile
    
    * Update Dockerfile
    
    * Update endpointslice_test.go
    
    * Redesign metric
    
    * Update endpointslice.go
    
    Remove unneeded label
    
    * Update endpointslice.go
    
    * Update endpointslice-metrics.md
    
    * Fix test
    
    * Remove hostname
    
    * Update docs/endpointslice-metrics.md
    
    ---------
    
    Co-authored-by: Manuel Rüger <manuel@rueg.eu>
    Co-authored-by: Damien Grisonnet <damien.grisonnet@epita.fr>

--- internal/store/endpointslice_test.go
+++ internal/store/endpointslice_test.go
@@ -113,9 +113,49 @@ func TestEndpointSliceStore(t *testing.T) {
 			},
 			Want: `
 					# HELP kube_endpointslice_endpoints Endpoints attached to the endpointslice.
+					# HELP kube_endpointslice_endpoints_hints Topology routing hints attached to endpoints
 					# TYPE kube_endpointslice_endpoints gauge
+					# TYPE kube_endpointslice_endpoints_hints gauge
 					kube_endpointslice_endpoints{address="10.0.0.1",endpoint_nodename="node",endpoint_zone="west",endpointslice="test_endpointslice-endpoints",hostname="host",ready="true",terminating="false"} 1
 					kube_endpointslice_endpoints{address="192.168.1.10",endpoint_nodename="node",endpoint_zone="west",endpointslice="test_endpointslice-endpoints",hostname="host",ready="true",terminating="false"} 1
+				  `,
+
+			MetricNames: []string{
+				"kube_endpointslice_endpoints",
+			},
+		},
+		{
+			Obj: &discoveryv1.EndpointSlice{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "test_endpointslice-endpoints",
+				},
+				AddressType: "IPv4",
+				Endpoints: []discoveryv1.Endpoint{
+					{
+						NodeName: &nodename,
+						Conditions: discoveryv1.EndpointConditions{
+							Ready:       &ready,
+							Terminating: &terminating,
+						},
+						Hostname:  &hostname,
+						Zone:      &zone,
+						Addresses: addresses,
+						Hints: &discoveryv1.EndpointHints{
+							ForZones: []discoveryv1.ForZone{
+								{Name: "zone1"},
+							},
+						},
+					},
+				},
+			},
+			Want: `
+					# HELP kube_endpointslice_endpoints Endpoints attached to the endpointslice.
+					# HELP kube_endpointslice_endpoints_hints Topology routing hints attached to endpoints
+					# TYPE kube_endpointslice_endpoints gauge
+        			# TYPE kube_endpointslice_endpoints_hints gauge
+         			kube_endpointslice_endpoints_hints{address="10.0.0.1",endpointslice="test_endpointslice-endpoints",for_zone="zone1"} 1
+        			kube_endpointslice_endpoints{address="10.0.0.1",endpoint_nodename="node",endpoint_zone="west",endpointslice="test_endpointslice-endpoints",hostname="host",ready="true",terminating="false"} 1
+        			kube_endpointslice_endpoints{address="192.168.1.10",endpoint_nodename="node",endpoint_zone="west",endpointslice="test_endpointslice-endpoints",hostname="host",ready="true",terminating="false"} 1
 				  `,
 
 			MetricNames: []string{

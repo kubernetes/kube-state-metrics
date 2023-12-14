ed1a7f992ce48056ab0eeb5ac39ee39cd8a4b0aa
changhyuni <bhs9610@naver.com>
2023-12-14 22:51:38 +0900

    pods: update init_cotainer_info metric

--- internal/store/pod.go
+++ internal/store/pod.go
@@ -680,13 +680,18 @@ func createPodInitContainerInfoFamilyGenerator() generator.FamilyGenerator {
 			labelKeys := []string{"container", "image_spec", "image", "image_id", "container_id", "type"}
 
 			for _, c := range p.Spec.InitContainers {
+				restartPolicy := ""
+				if c.RestartPolicy != nil {
+					restartPolicy = string(*c.RestartPolicy)
+				}
+
 				for _, cs := range p.Status.InitContainerStatuses {
 					if cs.Name != c.Name {
 						continue
 					}
 					ms = append(ms, &metric.Metric{
 						LabelKeys:   labelKeys,
-						LabelValues: []string{cs.Name, c.Image, cs.Image, cs.ImageID, cs.ContainerID, c.RestartPolicy},
+						LabelValues: []string{cs.Name, c.Image, cs.Image, cs.ImageID, cs.ContainerID, restartPolicy},
 						Value:       1,
 					})
 				}

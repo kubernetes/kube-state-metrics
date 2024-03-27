2a06c450615c4f4a908df043a54eba9f54cb459d
Pranshu Srivastava <rexagod@gmail.com>
2024-03-27 23:53:23 +0530

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fix: fallback to `gauge` for `protofmt`-based negotiations

--- pkg/metricshandler/metrics_handler.go
+++ pkg/metricshandler/metrics_handler.go
@@ -188,8 +188,8 @@ func (m *MetricsHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	contentType := expfmt.NegotiateIncludingOpenMetrics(r.Header)
 
-	// Assume text/plain if no Content-Type header is set.
-	if contentType == expfmt.FmtUnknown {
+	// We do not support protobuf at the moment. Fall back to FmtText if the negotiated exposition format is not FmtOpenMetrics See: https://github.com/kubernetes/kube-state-metrics/issues/2022.
+	if contentType != expfmt.FmtOpenMetrics_1_0_0 && contentType != expfmt.FmtOpenMetrics_0_0_1 {
 		contentType = expfmt.FmtText
 	}
 	resHeader.Set("Content-Type", string(contentType))

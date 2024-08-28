643739d04a03d5263868194efc5d3f9664e158ee
Richa Banker <richabanker@google.com>
2024-08-27 20:11:39 -0700

    Add unit test

--- internal/store/job_test.go
+++ internal/store/job_test.go
@@ -213,6 +213,28 @@ func TestJobStore(t *testing.T) {
 		{
 			Obj: &v1batch.Job{
 				ObjectMeta: metav1.ObjectMeta{
+					Name:      "FailedJobWithNoConditions",
+					Namespace: "ns1",
+				},
+				Status: v1batch.JobStatus{
+					Failed: 1,
+				},
+				Spec: v1batch.JobSpec{
+					ActiveDeadlineSeconds: &ActiveDeadlineSeconds900,
+				},
+			},
+			Want: metadata + `
+				kube_job_owner{job_name="FailedJobWithNoConditions",namespace="ns1",owner_is_controller="",owner_kind="",owner_name=""} 1
+				kube_job_info{job_name="FailedJobWithNoConditions",namespace="ns1"} 1
+				kube_job_spec_active_deadline_seconds{job_name="FailedJobWithNoConditions",namespace="ns1"} 900
+				kube_job_status_active{job_name="FailedJobWithNoConditions",namespace="ns1"} 0
+				kube_job_status_failed{job_name="FailedJobWithNoConditions",namespace="ns1",reason=""} 1
+				kube_job_status_succeeded{job_name="FailedJobWithNoConditions",namespace="ns1"} 0
+`,
+		},
+		{
+			Obj: &v1batch.Job{
+				ObjectMeta: metav1.ObjectMeta{
 					Name:       "SuccessfulJob2NoActiveDeadlineSeconds",
 					Namespace:  "ns1",
 					Generation: 1,

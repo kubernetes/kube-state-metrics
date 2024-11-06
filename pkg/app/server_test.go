b0e9be550b640538fdb15b8149292ac2ecd3fc51
Manuel Rüger <manuel@rueg.eu>
2024-11-06 09:10:20 +0100

    fix: Remove log output from benchmark
    Without this change Info logs appear in the benchmark
    
    ok      k8s.io/kube-state-metrics/v2/pkg/allowdenylist  0.005s
    I1106 08:50:22.006480  144965 builder.go:280] "Active resources" activeStoreNames="certificatesigningrequests,configmaps,cronjobs,daemonsets,deployments,endpoints,horizontalpodautoscalers,ingresses,jobs,leases,limitranges,mutatingwebhookconfigurations,namespaces,networkpolicies,nodes,persistentvolumeclaims,persistentvolumes,poddisruptionbudgets,pods,replicasets,replicationcontrollers,resourcequotas,secrets,services,statefulsets,storageclasses,validatingwebhookconfigurations,volumeattachments"
    goos: linux
    goarch: amd64
    pkg: k8s.io/kube-state-metrics/v2/pkg/app
    cpu: Intel(R) Core(TM) i7-1065G7 CPU @ 1.30GHz
    BenchmarkKubeStateMetrics/GenerateMetrics-8                    1        1001291639 ns/op        67578008 B/op     582810 allocs/op

--- pkg/app/server_test.go
+++ pkg/app/server_test.go
@@ -19,6 +19,7 @@ package app
 import (
 	"bytes"
 	"context"
+	"flag"
 	"fmt"
 	"io"
 	"net/http/httptest"
@@ -40,6 +41,7 @@ import (
 	"k8s.io/client-go/kubernetes/fake"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/cache"
+	"k8s.io/klog/v2"
 	samplev1alpha1 "k8s.io/sample-controller/pkg/apis/samplecontroller/v1alpha1"
 	samplefake "k8s.io/sample-controller/pkg/generated/clientset/versioned/fake"
 
@@ -70,6 +72,10 @@ func BenchmarkKubeStateMetrics(b *testing.B) {
 		b.Errorf("error injecting resources: %v", err)
 	}
 	ctx, cancel := context.WithCancel(context.Background())
+
+	klogFlags := flag.NewFlagSet("klog", flag.ExitOnError)
+	klog.InitFlags(klogFlags)
+	klogFlags.Set("logtostderr", "false")
 	defer cancel()
 	reg := prometheus.NewRegistry()
 

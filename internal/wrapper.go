9f31b1ebf6988a5ad877733f4b1c5b91d155fe9b
yshngg <yshngg@outlook.com>
2025-07-01 11:36:33 +0800

    changes for continuing to run when file doesnt exist

--- internal/wrapper.go
+++ internal/wrapper.go
@@ -43,17 +43,25 @@ func RunKubeStateMetricsWrapper(opts *options.Options) {
 	}
 
 	ctx, cancel := context.WithCancel(context.Background())
+
 	if file := options.GetConfigFile(*opts); file != "" {
 		cfgViper := viper.New()
 		cfgViper.SetConfigType("yaml")
 		cfgViper.SetConfigFile(file)
-		if err := cfgViper.ReadInConfig(); err != nil {
-			if errors.Is(err, viper.ConfigFileNotFoundError{}) {
-				klog.ErrorS(err, "Options configuration file not found", "file", file)
+		var cfgViperReadInConfigErr error
+		if cfgViperReadInConfigErr = cfgViper.ReadInConfig(); cfgViperReadInConfigErr != nil {
+			if errors.Is(cfgViperReadInConfigErr, viper.ConfigFileNotFoundError{}) {
+				klog.ErrorS(cfgViperReadInConfigErr, "Options configuration file not found at startup", "file", file)
+			} else if _, isNotExisterr := os.Stat(filepath.Clean(file)); isNotExisterr != nil {
+				// TODO: Remove this check once viper.ConfigFileNotFoundError is working as expected, see this issue -
+				// https://github.com/spf13/viper/issues/1783
+				klog.ErrorS(isNotExisterr, "Options configuration file not found at startup", "file", file)
 			} else {
-				klog.ErrorS(err, "Error reading options configuration file", "file", file)
+				klog.ErrorS(cfgViperReadInConfigErr, "Error reading options configuration file", "file", file)
 			}
-			klog.FlushAndExit(klog.ExitFlushTimeout, 1)
+			if !opts.ContinueWithoutConfig {
+				klog.FlushAndExit(klog.ExitFlushTimeout, 1)
+			}
 		}
 		cfgViper.OnConfigChange(func(e fsnotify.Event) {
 			klog.InfoS("Changes detected", "name", e.Name)
@@ -65,25 +73,33 @@ func RunKubeStateMetricsWrapper(opts *options.Options) {
 		})
 		cfgViper.WatchConfig()
 
-		// Merge configFile values with opts so we get the CustomResourceConfigFile from config as well
-		configFile, err := os.ReadFile(filepath.Clean(file))
-		if err != nil {
-			klog.ErrorS(err, "failed to read options configuration file", "file", file)
-		}
+		if cfgViperReadInConfigErr == nil {
+			// Merge configFile values with opts so we get the CustomResourceConfigFile from config as well
+			configFile, err := os.ReadFile(filepath.Clean(file))
+			if err != nil {
+				klog.ErrorS(err, "failed to read options configuration file", "file", file)
+			}
 
-		yaml.Unmarshal(configFile, opts)
+			yaml.Unmarshal(configFile, opts)
+		}
 	}
 	if opts.CustomResourceConfigFile != "" {
 		crcViper := viper.New()
 		crcViper.SetConfigType("yaml")
 		crcViper.SetConfigFile(opts.CustomResourceConfigFile)
-		if err := crcViper.ReadInConfig(); err != nil {
-			if errors.Is(err, viper.ConfigFileNotFoundError{}) {
-				klog.ErrorS(err, "Custom resource configuration file not found", "file", opts.CustomResourceConfigFile)
+		if cfgViperReadInConfigErr := crcViper.ReadInConfig(); cfgViperReadInConfigErr != nil {
+			if errors.Is(cfgViperReadInConfigErr, viper.ConfigFileNotFoundError{}) {
+				klog.ErrorS(cfgViperReadInConfigErr, "Custom resource configuration file not found at startup", "file", opts.CustomResourceConfigFile)
+			} else if _, isNotExisterr := os.Stat(filepath.Clean(opts.CustomResourceConfigFile)); isNotExisterr != nil {
+				// Adding this check in addition to the above since viper.ConfigFileNotFoundError is not working as expected due to this issue -
+				// https://github.com/spf13/viper/issues/1783
+				klog.ErrorS(isNotExisterr, "Custom resource configuration file not found at startup", "file", opts.CustomResourceConfigFile)
 			} else {
-				klog.ErrorS(err, "Error reading Custom resource configuration file", "file", opts.CustomResourceConfigFile)
+				klog.ErrorS(cfgViperReadInConfigErr, "Error reading Custom resource configuration file", "file", opts.CustomResourceConfigFile)
 			}
-			klog.FlushAndExit(klog.ExitFlushTimeout, 1)
+			if !opts.ContinueWithoutCustomResourceConfigFile {
+				klog.FlushAndExit(klog.ExitFlushTimeout, 1)
+			}
 		}
 		crcViper.OnConfigChange(func(e fsnotify.Event) {
 			klog.InfoS("Changes detected", "name", e.Name)

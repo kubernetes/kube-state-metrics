cffd506c6d71ff6070489288012e967e99a38f20
Soumya <somuraik@gmail.com>
2025-08-11 12:49:50 +0530

    fix: Format imports in deployment_test.go

--- internal/store/deployment_test.go
+++ internal/store/deployment_test.go
@@ -24,6 +24,7 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
+	
 	generator "k8s.io/kube-state-metrics/v2/pkg/metric_generator"
 )
 

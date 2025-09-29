63b74d040537417647dfa84ccd5ba7dfad329691
Manuel Rüger <manuel@rueg.eu>
2025-09-29 12:33:49 +0200

    chore: Bump to kubernetes 1.34

--- tests/e2e.sh
+++ tests/e2e.sh
@@ -25,13 +25,13 @@ case $(uname -m) in
 esac
 
 NODE_IMAGE_NAME="docker.io/kindest/node"
-KUBERNETES_VERSION=${KUBERNETES_VERSION:-"v1.33.4"}
+KUBERNETES_VERSION=${KUBERNETES_VERSION:-"v1.34.0"}
 KUBE_STATE_METRICS_LOG_DIR=./log
 KUBE_STATE_METRICS_CURRENT_IMAGE_NAME="registry.k8s.io/kube-state-metrics/kube-state-metrics"
 KUBE_STATE_METRICS_IMAGE_NAME="registry.k8s.io/kube-state-metrics/kube-state-metrics-${ARCH}"
 E2E_SETUP_KIND=${E2E_SETUP_KIND:-}
 E2E_SETUP_KUBECTL=${E2E_SETUP_KUBECTL:-}
-KIND_VERSION=v0.29.0
+KIND_VERSION=v0.30.0
 SUDO=${SUDO:-}
 
 OS=$(uname -s | awk '{print tolower($0)}')

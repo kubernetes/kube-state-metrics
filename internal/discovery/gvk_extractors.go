a0367caba321084b3a95da6610b9d0ccfd00d56e
Alexander North <alexandernorth@users.noreply.github.com>
2026-02-11 16:07:35 +0100

    split pr: remove APIService discovery components

--- internal/discovery/gvk_extractors.go
+++ internal/discovery/gvk_extractors.go
@@ -14,13 +14,8 @@ limitations under the License.
 package discovery
 
 import (
-	"fmt"
-	"strings"
-
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime/schema"
-	k8sdiscovery "k8s.io/client-go/discovery"
-	"k8s.io/klog/v2"
 )
 
 type crdExtractor struct{}
@@ -49,88 +44,5 @@ func (e *crdExtractor) ExtractGVKs(obj interface{}) []*DiscoveredResource {
 			Plural: p,
 		})
 	}
-	return resources
-}
-
-func isAPIServiceReady(obj interface{}) bool {
-	status, found, err := unstructured.NestedSlice(obj.(*unstructured.Unstructured).Object, "status", "conditions")
-	if err != nil || !found {
-		return false
-	}
-
-	for _, condition := range status {
-		conditionMap, ok := condition.(map[string]interface{})
-		if !ok {
-			continue // skip invalid condition
-		}
-		if conditionMap["type"] == "Available" && conditionMap["status"] == "True" {
-			return true
-		}
-	}
-	return false
-}
-
-type apiServiceExtractor struct {
-	discoveryClient *k8sdiscovery.DiscoveryClient
-}
-
-// SourceID returns a unique identifier for the APIService.
-func (e *apiServiceExtractor) SourceID(obj interface{}) string {
-	u := obj.(*unstructured.Unstructured)
-	return "apiservice:" + u.GetName()
-}
-
-// ExtractGVKs extracts GVK information from an APIService object.
-// Returns nil if the APIService is not ready (signals "skip update").
-func (e *apiServiceExtractor) ExtractGVKs(obj interface{}) []*DiscoveredResource {
-	serviceSpec := obj.(*unstructured.Unstructured).Object["spec"].(map[string]interface{})
-	group, _, err := unstructured.NestedString(serviceSpec, "group")
-	if err != nil {
-		klog.ErrorS(err, "failed to extract group from APIService")
-		return nil
-	}
-	version, _, err := unstructured.NestedString(serviceSpec, "version")
-	if err != nil {
-		klog.ErrorS(err, "failed to extract version from APIService")
-		return nil
-	}
-
-	// Check if APIService has a service defined - i.e. not local
-	if svc, ok := serviceSpec["service"]; !ok || svc == nil {
-		klog.V(5).InfoS("skipping local APIService", "group", group, "version", version)
-		// Return empty slice to clear any existing resources for this source
-		return []*DiscoveredResource{}
-	}
-
-	if !isAPIServiceReady(obj) {
-		klog.InfoS("skipping non-ready APIService", "group", group, "version", version)
-		// Return empty slice to remove resources for non-ready APIService
-		return []*DiscoveredResource{}
-	}
-
-	resourceList, err := e.discoveryClient.ServerResourcesForGroupVersion(fmt.Sprintf("%s/%s", group, version))
-	if err != nil {
-		klog.ErrorS(err, "failed to fetch server resources for group version", "groupVersion", fmt.Sprintf("%s/%s", group, version))
-		// Return nil to skip resources update
-		return nil
-	}
-
-	var resources []*DiscoveredResource
-	for _, resource := range resourceList.APIResources {
-		// Skip subresources
-		if strings.Contains(resource.Name, "/") {
-			continue
-		}
-
-		resources = append(resources, &DiscoveredResource{
-			GroupVersionKind: schema.GroupVersionKind{
-				Group:   group,
-				Version: version,
-				Kind:    resource.Kind,
-			},
-			Plural: resource.Name,
-		})
-	}
-
 	return resources
 }
